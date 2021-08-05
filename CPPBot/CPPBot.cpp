9439630fbb67fc11c5a89d62408e0db5fa9fa71a
inf <76598833+inf7@users.noreply.github.com>
2021-08-05 08:18:17 +0300

    Add files via upload

---
+++ CPPBot/CPPBot.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <future>
+#include <thread>
+#include <chrono>
+#include "corefunc.h"
+#include "userfunc.h"
+#include "json.hpp"
+#include <string>
+#include <windows.h>
+#include <direct.h>
+#define GetCurrentDir _getcwd
+
+using namespace std;
+using json = nlohmann::json;
+
+vector<GrowtopiaBot> bots;
+
+inline bool exists_test(const string& name) {
+	ifstream f(name.c_str());
+	return f.good();
+}
+
+int main() {
+	SetConsoleTitleA("CPPBot By DrOreo002 Fixed by .Lucy#0085");
+	init();
+	char cCurrentPath[FILENAME_MAX];
+
+	if (!GetCurrentDir(cCurrentPath, sizeof(cCurrentPath)))
+	{
+		return errno;
+	}
+
+	cCurrentPath[sizeof(cCurrentPath) - 1] = '\0';
+	string botDataPath = cCurrentPath + string("\\bot.json");
+
+	if (!exists_test(botDataPath)) {
+		cout << "Cannot find bot data on that path!. Stuck?, please read README.md" << endl;
+		return 0;
+	}
+	cout << "Loading bot data on path (" << botDataPath << ")" << endl;
+
+	ifstream i(botDataPath);
+	json j;
+	i >> j;
+
+	GrowtopiaBot bot = {
+		j["username"], j["password"]
+	};
+	cout << "------------------------" << endl;
+	cout << "Bot loaded!. Using username " << j["username"].get<string>() << ", With owner " + j["ownerUsername"].get<string>() << endl;
+
+	bot.gameVersion = j["gameVersion"].get<string>();
+	bot.worldName = j["targetWorld"].get<string>();
+	bot.ownerUsername = j["ownerUsername"].get<string>();
+
+	bot.userInit();
+	bots.push_back(bot);
+
+	while (true) {
+		bot.eventLoop();
+		bot.userLoop();
+	}
+}
